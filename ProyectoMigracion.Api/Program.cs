db4c54ff894c545016269ee94df170595e0d297c
Braylin <braylinvasquez20@gmail.com>
2024-03-01 16:46:40 -0400

    Inyectando el contexto

--- ProyectoMigracion.Api/Program.cs
+++ ProyectoMigracion.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoMigracion.Api.Dal;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(op => op.UseSqlite(ConStr));
 
 var app = builder.Build();
 
