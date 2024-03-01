43dc561eb121cceaa9b331e4e149ad9ac4de99d0
Braylin <braylinvasquez20@gmail.com>
2024-03-01 16:46:01 -0400

    Creando el contexto

---
+++ ProyectoMigracion.Api/Dal/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace ProyectoMigracion.Api.Dal
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options): base(options) { }
+        public DbSet<Prioridades> Prioridades { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Tickets> Tickets { get; set; }
+    }
+}
